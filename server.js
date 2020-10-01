925a794d8aca6fbca086c1dea0e3e30493182719
jaijain230999 <jai.j@somaiya.edu>
2020-10-01 11:06:25 +0530

    routes added

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
 // Connection Initialized to DB
 connectDB()
 
-app.get('/', (req, res) => {
-  res.json({
-    data: true,
-  });
-});
+// Define Routes
+app.use('/devconnector/api/v1/users', require('./routes/api/users'))
+app.use('/devconnector/api/v1/auth', require('./routes/api/auth'))
+app.use('/devconnector/api/v1/profile', require('./routes/api/profile'))
+app.use('/devconnector/api/v1/posts', require('./routes/api/posts'))
 
 const PORT = process.env.PORT || 5000;
 
