f0cecaf8b29404468e6d4c19e0f5ccccefbd7ce5
jaijain230999 <jai.j@somaiya.edu>
2020-10-01 10:50:45 +0530

    DB Connected

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const connectDB = require('./config/db')
 
 const app = express();
+
+// Connection Initialized to DB
+connectDB()
 
 app.get('/', (req, res) => {
   res.json({
