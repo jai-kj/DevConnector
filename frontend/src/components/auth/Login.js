b132cc0626b2aaa44505b192f66f0e175c9af0ed
jaijain230999 <jai.j@somaiya.edu>
2020-10-02 13:41:58 +0530

    Register and Login Form done

--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -1,16 +1,57 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+// import PropTypes from 'prop-types'
 
 const Login = props => {
+  const [ formData, setFormData ] = useState({
+    email: '',
+    password: ''
+  })
+
+  const { email, password } = formData
+
+  const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value })
+
+  const onSubmit = e => {
+    e.preventDefault()
+    console.log(formData);
+  }
   return (
-    <div>
-      Login
-    </div>
+    <>
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead"><i className="fas fa-user" /> Sign into Your Account</p>
+      <form className="form" onSubmit={onSubmit}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={onChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={onChange}
+            required
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign Up</Link>
+      </p>
+    </>
   )
 }
 
-Login.propTypes = {
+// Login.propTypes = {
 
-}
+// }
 
 export default Login
--- frontend/src/components/auth/Register.js
+++ frontend/src/components/auth/Register.js
@@ -1,16 +1,92 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+// import PropTypes from 'prop-types'
 
 const Register = props => {
+
+  const [ formData, setFormData ] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  })
+
+  const { name, email, password, password2 } = formData
+
+  const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value })
+
+  const onSubmit = e => {
+    e.preventDefault()
+    if(password !== password2) {
+      console.log("Password doesn't match!");
+      return
+    }
+
+    console.log(formData);
+  }
+
   return (
-    <div>
-      Register
-    </div>
+    <>
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead"><i className="fas fa-user" /> Create Your Account</p>
+      <form className="form" onSubmit={onSubmit}>
+        <div className="form-group">
+          <input 
+            type="text" 
+            placeholder="Name" 
+            name='name' 
+            value={name}
+            onChange={onChange}
+            required 
+          />
+        </div>
+        <div className="form-group">
+          <input 
+            type="email" 
+            placeholder="Email Address" 
+            name='email'
+            value={email} 
+            onChange={onChange}
+            required 
+          />
+          <small className="form-text">
+            This site uses Gravatar so if you want a profile image, use a
+            Gravatar email
+          </small>
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={onChange}
+            minLength="6"
+            required 
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name='password2'
+            value={password2}
+            onChange={onChange}
+            minLength="6"
+            required 
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+      <p className="my-1">
+        Already have an account? <Link to="/login">Sign In</Link>
+      </p>
+    </>
   )
 }
 
-Register.propTypes = {
+// Register.propTypes = {
 
-}
+// }
 
 export default Register
